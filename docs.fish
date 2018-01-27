895c9b8155b0f4da47dd954c3753f3d066f7e21e
John Soo <john.soo@verys.com>
2018-01-26 20:43:05 -0800

    Add a cache file to speed it up some

--- docs.fish
+++ docs.fish
@@ -1,7 +1,12 @@
-cf h -a | \
-grep -vE '^([ ]+)?(\x{1b}.+[A-Z]+|cf|[0-9]+|[ ]*$|[a-zA-Z_]+=|-)' | \
-sed -E 's/[ ]+([a-zA-Z0-9-]+)[ ]+([a-zA-Z0-9].*)/complete -c cf -a \1 -d "\2"/' | \
-while read -l line
-  eval $line
+if test -e ~/.config/fish/completions/cloud-foundry/.cf-completions
+  source ~/.config/fish/completions/cloud-foundry/.cf-completions
+else
+  cf h -a | \
+  grep -vE '^([ ]+)?(\x{1b}.+[A-Z]+|cf|[0-9]+|[ ]*$|[a-zA-Z_]+=|-)' | \
+  sed -E 's/[ ]+([a-zA-Z0-9-]+)[ ]+([a-zA-Z0-9].*)/complete -c cf -f -a \1 -d "\2"/' | \
+  while read -l line
+    eval $line
+    echo $line >> ~/.config/fish/completions/cloud-foundry/.cf-completions
+  end
 end
 
